ff6894f87d5a8f8917d38aa784be4c34b5a1bcd7
njsm8 <njmshetty@gmail.com>
2019-02-11 00:09:49 +0530

    second commit

---
+++ Kaggle/titanic_kaggle.R
@@ -0,0 +1,6 @@
+#setting working directory
+setwd("C:/Users/Nischit Shetty/Desktop/Kaggle")
+
+#loading datasets
+titanic.train <- read.csv(file = "train.csv", stringsAsFactors = FALSE, header = TRUE)
+titanic.test <-read
